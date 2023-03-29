81e57dfe32154331646352d007e21a5cd974270a
BKSaint <113442572+BKSaint@users.noreply.github.com>
2023-03-29 10:55:28 -0400

    signup sheet

--- main.py
+++ main.py
@@ -17,11 +17,28 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM `posts` ORDER BY `timestamp`")
+    cursor.execute("SELECT * FROM `posts` JOIN `users` ON `posts`.`user_id` = `users`.`id` ORDER BY `timestamp` DESC" )
     results = cursor.fetchall()
 
     return render_template("main.html.jinja", posts=results)
 
+@app.route("/signup", methods=['POST', 'GET'])
+def signup():
+    
+    if request.method == 'POST':
+        cursor = connection.cursor()
+        cursor.execute("""
+            INSERT INTO `users` (`username`, `display_name`, `password`, `email`, `bio`, 
+            `birthday`, `pfp`) VALUES ('%s, %s, %s, %s, %s, %s, %s')
+        """)
+
+        return request.form
+    elif request.method == 'GET':
+        return render_template("signup.html.jinja")
+
+@app.route("/login")
+def login():
+    return render_template("login.html.jinja")
 
 if __name__ == '__main__':
     app.run(debug=True)
