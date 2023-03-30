0bc004770964cc3665294257bbc230d301c2284d
BKSaint <113442572+BKSaint@users.noreply.github.com>
2023-03-30 10:51:31 -0400

    started login process

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 from flask import Flask, render_template, request, redirect
+from flask_login import LoginManager
 import pymysql
 import pymysql.cursors
 
+login_manager = LoginManager()
+
+app = Flask(__name__)
+login_manager.init_app(app)
+
+class User():
+    def __init__(self, id, username, banned):
+        self.is_authenticated = True
+        self.is_anonymous = False
+        self.is_active = not banned
+
+        self.username = username
+        self.id = id
+
+    def get_id(self):
+        return(str(self.id))
+        
+    
+@login_manager.user_loader
+def user_loader(user_id):
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM `users` WHERE `id` = " + user_id)
+    result = cursor.fetchone()
+    if result is None:
+        return None
+
+    return User(result['id'], result['username'], result['banned'])
 
 connection = pymysql.connect(
     host="10.100.33.60",
@@ -10,10 +38,8 @@ connection = pymysql.connect(
     database="swalker_appdatabase",
     cursorclass=pymysql.cursors.DictCursor,
     autocommit=True
-
 )
 
-app = Flask(__name__)
 @app.route("/")
 def home():
     cursor = connection.cursor()
@@ -27,12 +53,23 @@ def signup():
     
     if request.method == 'POST':
         cursor = connection.cursor()
+        
+        photo = request.files['pfp']
+        file_name = photo.filename
+        file_extension = file_name.split('.')[-1]
+
+        if file_extension in ['jpg', 'jpeg', 'png', 'gif']:
+            photo.save('media/users/' + file_name)
+        else:
+            raise Exception('Invalid File Type')
+
+
         cursor.execute("""
             INSERT INTO `users` (`username`, `display_name`, `password`, `email`, `bio`, 
-            `birthday`, `pfp`) VALUES ('%s, %s, %s, %s, %s, %s, %s')
-        """)
+            `birthday`, `pfp`) VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """, (request.form['username'], request.form['display_name'], request.form['password'], request.form['email'], request.form['bio'], request.form['birthday'], file_name))
 
-        return request.form
+        return redirect('/')
     elif request.method == 'GET':
         return render_template("signup.html.jinja")
 
